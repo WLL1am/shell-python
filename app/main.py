2613ae8af9dcf48c1bfa81cf262b580d090e81d3
William Chung <118134309+WLL1am@users.noreply.github.com>
2025-02-03 12:04:19 -0800

    implemented echo and type builtin

--- app/main.py
+++ app/main.py
@@ -1,13 +1,31 @@
 import sys
+import os
 
 def main():
     
+    builtin_cmds = ["echo", "exit", "type"]
+    PATH = os.environ.get("PATH")
+    
     while True:
         sys.stdout.write("$ ")
-        cmd = input()
-        if cmd == "exit 0":
-            return 0
-        print(f"{cmd}: command not found")
+        sys.stdout.flush()
+        
+        # Retrive and parse user input
+        cmd_params = input()
+        cmd_params_list = cmd_params.split(" ")
+        
+        match cmd_params_list:
+            case ["type", arg] if arg in builtin_cmds:
+                print(f"{arg} is a shell builtin")
+            case ["type", arg] if arg not in builtin_cmds:
+                print(f"{arg}: not found")
+            case ["exit", "0"]:
+                return 0
+            case ["echo", *args]:
+                print(*args)
+            case _:
+                print(f"{cmd_params_list[0]}: command not found")
+        
     
     
 if __name__ == "__main__":
