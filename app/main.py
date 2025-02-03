8fa2e8fbe4b463eb6298a0b0996f00d975d2bb8f
William Chung <118134309+WLL1am@users.noreply.github.com>
2025-02-03 12:16:30 -0800

    implemented PATH finding for type

--- app/main.py
+++ app/main.py
@@ -15,18 +15,26 @@ def main():
         cmd_params_list = cmd_params.split(" ")
         
         match cmd_params_list:
-            case ["type", arg] if arg in builtin_cmds:
-                print(f"{arg} is a shell builtin")
-            case ["type", arg] if arg not in builtin_cmds:
-                print(f"{arg}: not found")
             case ["exit", "0"]:
                 return 0
             case ["echo", *args]:
                 print(*args)
+            case ["type", arg]:
+                cmd = cmd_params_list[1]
+                cmd_path = None
+                paths = PATH.split(":")
+                for path in paths:
+                    if os.path.isfile(f"{path}/{cmd}"):
+                        cmd_path = f"{path}/{cmd}"
+                
+                if cmd in builtin_cmds:
+                    print(f"{cmd} is a shell builtin")
+                elif cmd_path:
+                    print(f"{cmd} is {cmd_path}")
+                else:
+                    print(f"{cmd} not found")
             case _:
                 print(f"{cmd_params_list[0]}: command not found")
         
-    
-    
 if __name__ == "__main__":
     main()
